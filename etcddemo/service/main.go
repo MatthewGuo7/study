56774708fc7f2e4439e4c7bff47d6fcd94b2afaa
zhonghui.guo <zhonghui.guo@wenba100.com>
2020-09-18 19:09:29 +0800

    udpate

--- etcddemo/service/main.go
+++ etcddemo/service/main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"context"
 	"etcddemo/register"
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+	"github.com/satori/go.uuid"
 )
 
 func main() {
+	name := flag.String("name", "", "服务名称")
+	port := flag.Int("p", 0, "服务端口")
+	flag.Parse()
+	if *name == "" {
+		log.Fatal("请指定服务名")
+	}
+	if *port == 0 {
+		log.Fatal("请指定端口")
+	}
+
 	route := mux.NewRouter()
 	route.HandleFunc("/prod/{id:\\d+}", func(writer http.ResponseWriter, request *http.Request) {
 		vars := mux.Vars(request)
@@ -20,9 +33,13 @@ func main() {
 		writer.Write([]byte(str))
 	})
 
-	serviceID := "p1"
+	serviceName := *name
+	serviceAddr := "127.0.0.1"
+	servicePort := *port
+	serviceID := uuid.NewV4().String()
+
 	rc := register.NewRegisterService([]string{"127.0.0.1:2379"})
-	err := rc.RegService(serviceID, "productservice", "127.0.0.1:8081")
+	err := rc.RegService(serviceID, serviceName, serviceAddr+":"+strconv.Itoa(servicePort))
 	if nil != err {
 		log.Fatal(err)
 	}
@@ -31,7 +48,7 @@ func main() {
 		fmt.Println(rc.UnRegister(serviceID))
 	}()
 
-	httpServer := &http.Server{Addr: ":8081", Handler: route}
+	httpServer := &http.Server{Addr: ":" + strconv.Itoa(servicePort), Handler: route}
 
 	go func() {
 		err = httpServer.ListenAndServe()
@@ -44,6 +61,6 @@ func main() {
 	sig := make(chan os.Signal)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	select {
-	case <- sig:
+	case <-sig:
 	}
 }
